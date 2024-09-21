ea575ea73cf5e969f4c6d814b6b09c76f733a61d
Mehdi-ehsani <metti.work@gmail.com>
2024-09-21 21:36:31 +0330

    add contacts and addContatcts component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import ContactsProvider from "./context/ContactsProvider"
-import Test from "./Test"
-
+import Navbar from "./components/Navbar";
+import ContactsProvider from "./context/ContactsProvider";
+import ActiveTabProvider  from "./context/ActiveTabProvider";
+import Test from "./Test";
+import ActivePage from "./components/ActivePage";
 
 function App() {
-  return (
-    <ContactsProvider>
-     <Test/>
-    </ContactsProvider>
-  )
+	return (
+		<ContactsProvider>
+			<ActiveTabProvider>
+				<Navbar />
+				<ActivePage/>
+				<Test />
+			</ActiveTabProvider>
+		</ContactsProvider>
+	);
 }
 
-export default App
+export default App;
