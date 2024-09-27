2ae4004c93862f698370db8cf65977cf49fa0625
Mehdi-ehsani <metti.work@gmail.com>
2024-09-27 18:24:14 +0330

    change context to localStorage

--- src/context/ContactsProvider.jsx
+++ src/context/ContactsProvider.jsx
@@ -1,34 +1,45 @@
-import {createContext , useReducer , useEffect} from 'react'
-import axios from 'axios';
+import { createContext, useReducer, useEffect } from 'react';
 
 export const ContactsContext = createContext();
+
 const initialState = {
     isLoading: true,
     data: [],
     error: ""
-}
-const reducer = (state , action) => {
-  switch(action.type) {
-    case "SUCCES":
-        return {isLoading: false , data: action.payload , error: ""}
-    case "FAILED":
-        return {isLoading: false , data: [] , error: action.payload}   
-  }
-}
+};
 
-const ContactsProvider = ({children}) => {
-  const [contacts , dispatchContact] = useReducer(reducer , initialState); 
-  useEffect(() => {
-        axios.get("http://localhost:8000/contacts")
-           .then(({data}) => dispatchContact({type: "SUCCES" , payload: data}))
-           .catch(error => dispatchContact({type: "FAILED" , payload: error.message}))
-  },[contacts]) 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SUCCESS":
+            return { isLoading: false, data: action.payload, error: "" };
+        case "FAILED":
+            return { isLoading: false, data: [], error: action.payload };
+        default:
+            return state;
+    }
+};
 
-  return (
-    <ContactsContext.Provider value={[contacts , dispatchContact]}>
-        {children}
-    </ContactsContext.Provider>
-  )
-}
+const ContactsProvider = ({ children }) => {
+    const [contacts, dispatchContact] = useReducer(reducer, initialState);
 
-export default ContactsProvider
+    useEffect(() => {
+        try {
+            const storedContacts = localStorage.getItem('contacts');
+            if (storedContacts) {
+                dispatchContact({ type: "SUCCESS", payload: JSON.parse(storedContacts) });
+            } else {
+                dispatchContact({ type: "SUCCESS", payload: [] });
+            }
+        } catch (error) {
+            dispatchContact({ type: "FAILED", payload: error.message });
+        }
+    }, []);
+
+    return (
+        <ContactsContext.Provider value={[contacts, dispatchContact]}>
+            {children}
+        </ContactsContext.Provider>
+    );
+};
+
+export default ContactsProvider;
