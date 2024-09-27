e1d59451aec33aa2f8bc68749606ac3ff0444fa6
Mehdi-ehsani <metti.work@gmail.com>
2024-09-27 18:06:43 +0330

    add search input for search in contacts

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,43 +1,90 @@
-import React, { useContext ,useState } from "react";
+import React, { useContext, useState } from "react";
 import { ContactsContext } from "../../context/ContactsProvider";
 import ContactCard from "../contactCard/ContactCard";
-import styles from "./contacts.module.css"
+import styles from "./contacts.module.css";
 import axios from "axios";
 import Spinner from "../Spinner/Spinner";
-import emptyImg from "../../assets/image/empty.png"
+import emptyImg from "../../assets/image/empty.png";
+import searchImg from "../../assets/image/search.png";
 
 const Contacts = () => {
-	const [{ isLoading, data, error }, dispatchContact] = useContext(ContactsContext);
+	const [{ isLoading, data, error }, dispatchContact] =
+		useContext(ContactsContext);
 	const [selectedIds, setSelectedIds] = useState([]);
-	const [showCheckBox , setShowCheckBox] = useState(false)
-
+	const [showCheckBox, setShowCheckBox] = useState(false);
+	const [searchedValue , setSearchedValue] = useState("")
+	const [searchedData , setSearchedData] = useState([])
+    const searchHandler = (e) => {
+     const value = e.target.value.toLowerCase();
+	 setSearchedValue(value)
+	const filteredData =  data.filter(contact => {
+	return	contact.name.toLowerCase().includes(value) ||
+		contact.email.toLowerCase().includes(value)
+	 })
+	console.log(filteredData) 
+	setSearchedData(filteredData) 
+}
 	const handleCheckboxChange = (id) => {
 		setSelectedIds((prevSelectedIds) => {
-		  if (prevSelectedIds.includes(id)) {
-			return prevSelectedIds.filter((selectedId) => selectedId !== id);
-		  } else {
-			return [...prevSelectedIds, id];
-		  }
+			if (prevSelectedIds.includes(id)) {
+				return prevSelectedIds.filter((selectedId) => selectedId !== id);
+			} else {
+				return [...prevSelectedIds, id];
+			}
 		});
-	  };
-	  const deleteSelectedContacts = () => {
-		axios.all(selectedIds.map(id => axios.delete(`http://localhost:8000/contacts/${id}`)))
-		 .then(res => console.log("Delete All",res))
-		 .catch(error => console.log("Error",error))
+	};
+	const deleteSelectedContacts = () => {
+		axios
+			.all(
+				selectedIds.map((id) =>
+					axios.delete(`http://localhost:8000/contacts/${id}`)
+				)
+			)
+			.then((res) => console.log("Delete All", res))
+			.catch((error) => console.log("Error", error));
 		setSelectedIds([]);
-	  };  
+	};
 	return (
-		<div style={{position: "reletive"}}>
-			<div>{isLoading && <Spinner/>}</div>
-	        <div className={styles.btnContainer}>
-			<div>{selectedIds.length > 0 && <button className={styles.deleteAllBtn} onClick={deleteSelectedContacts}>Delete All</button>}</div>
-			<div>{!!data.length && <button className={styles.selectBtn} onClick={() => {setShowCheckBox(!showCheckBox); setSelectedIds([])}}>{showCheckBox ? "Deselect All" : "Select All"}</button>}</div>
+		<div style={{ position: "reletive" }}>
+			<div>{isLoading && <Spinner />}</div>
+			<div className={styles.btnContainer}>
+						{!!data.length && (
+						<div className={styles.searchInput}>
+							<input value={searchedValue} onChange={searchHandler} />
+							<img src={searchImg} alt="icon"/>
+						</div>
+						)}
+				<div className={styles.btnc}>
+					<div>
+						{selectedIds.length > 0 && (
+							<button
+								className={styles.deleteAllBtn}
+								onClick={deleteSelectedContacts}
+							>
+								Delete All
+							</button>
+						)}
+					</div>
+					<div>
+						{!!data.length && (
+							<button
+								className={styles.selectBtn}
+								onClick={() => {
+									setShowCheckBox(!showCheckBox);
+									setSelectedIds([]);
+								}}
+							>
+								{showCheckBox ? "Deselect All" : "Select All"}
+							</button>
+						)}
+					</div>
+				</div>
 			</div>
 			<div className={styles.contactsContainer}>
 				{!!data.length &&
-					data.map((contact) => (
+					(searchedValue ? searchedData : data).map((contact) => (
 						<ContactCard
-						    key={contact.id}
+							key={contact.id}
 							name={contact.name}
 							email={contact.email}
 							id={contact.id}
@@ -48,7 +95,9 @@ const Contacts = () => {
 						/>
 					))}
 			</div>
-			{data.length === 0 && !error && <img className={styles.emptyImg} src={emptyImg} />}
+			{data.length  === 0 && !error && !isLoading && (		
+				<img className={styles.emptyImg} src={emptyImg} />	
+			)}
 			<div>{!!error && <h1>{error}</h1>}</div>
 		</div>
 	);
